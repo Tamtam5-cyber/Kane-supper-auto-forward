383afc414e6bb2dc0440586f3c987682208a4f23
kane5 <Nguyenkhactam5@gmail.com>
2025-03-24 01:43:00 +0700

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Đang khởi động bot forwarding..."
-nohup python3 forwarder.py > forward.log 2>&1 &
-nohup python3 bot_control.py > bot.log 2>&1 &
-nohup python3 schedule.py > schedule.log 2>&1 &
-echo "Bot đã chạy!"
